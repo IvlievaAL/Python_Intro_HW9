15040e230528fa3ca588f2d5d2046ecf4ca810e5
AlexIv <ivlieva.medphyslab.com>
2023-06-07 12:09:29 +0300

    Z40 has been completed

--- zadacha40.py
+++ zadacha40.py
@@ -5,3 +5,5 @@ import pandas as pd
 
 
 df = pd.read_csv("california_housing_train.csv")
+df1 = df[df.population < 501] # ноль как нижняя граница не указан, т.к. население меньше 0 быть не может.
+print(df1.median_house_value.mean())
